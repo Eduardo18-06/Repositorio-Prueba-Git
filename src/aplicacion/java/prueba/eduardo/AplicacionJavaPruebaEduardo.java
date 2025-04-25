de190b9cabd52d058d5cd7a7d7491004b75cc61b
Eduardo Tasiguano <>
2025-04-24 23:27:57 -0500

    Primera modificacion del proyecto

---
+++ src/aplicacion/java/prueba/eduardo/AplicacionJavaPruebaEduardo.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aplicacion.java.prueba.eduardo;
+
+/**
+ *
+ * @author BODEGA
+ */
+public class AplicacionJavaPruebaEduardo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Primer mensaje prueba dentro del proyecto");
+    }
+    
+}
